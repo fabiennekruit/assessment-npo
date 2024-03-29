8beea02ed72833d4a62fef3485d14480e4945f85
Fabienne <fabienne_kruit@hotmail.com>
2024-03-29 16:59:34 +0100

    feat: Adjust type of TDetailPokemon

--- src/types/DetailPokemon.ts
+++ src/types/DetailPokemon.ts
@@ -1,7 +1,7 @@
 export type TDetailPokemon = {
   name: string;
-  sprites: {
-    back_default: string;
-    front_default: string;
-  };
+  id: number;
+  weight: number;
+  height: number;
+  images: string[];
 };
